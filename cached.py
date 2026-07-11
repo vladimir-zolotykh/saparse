11e6056f46e0ccd5952e4a12d636d8dbcd6dfdde
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-07-11 10:54:15 +0300

    Fix __call__, _repr__, _as_tuple()
    -        if cls not in (cached := type(cls).cached) and key not in cached[cls]:
    +        if cls not in (cached := type(cls).cached) or key not in cached[cls]:

--- cached.py
+++ cached.py
@@ -5,32 +5,32 @@ from collections import defaultdict
 
 
 class CachedMeta(type):
-    cached = defaultdict(defaultdict())
+    cached = defaultdict(defaultdict)
 
     def __call__(cls, *args, **kwargs):
-        # cached = type(cls).cached
         key = tuple(args)
-        if cls not in (cached := type(cls).cached) and key not in cached[cls]:
+        if cls not in (cached := type(cls).cached) or key not in cached[cls]:
             cached[cls][key] = super().__call__(*args, **kwargs)
         return cached[cls][key]
 
 
 class Person(metaclass=CachedMeta):
     def __init__(self, name, age, salary):
-        print(f"Initializing {type(self).__name__} object")
+        print(f"Initializing {type(self).__name__} object of {name!r}")
         self.name = name
         self.age = age
         self.salary = salary
 
-    def _as_tuple(self):
-        return tuple()
+    def _as_tuple(self) -> tuple[type, ...]:
+        return tuple(a for a in self.__dict__.values())
 
     def __repr__(self):
-        return f"{type(self).__name__}(self._as_tuple())"
+        return f"{type(self).__name__}{self._as_tuple()}"
 
 
 if __name__ == "__main__":
     bob = Person("Bob", 37, 12000)
+    print(bob)
     leo = Person("Leo", 42, 48000)
     bob = Person("Bob", 37, 12000)
     bob = Person("Bob", 38, 12000)
