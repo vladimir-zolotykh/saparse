ac907b0302daae564aadf20ac91ab9156af9f645
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-07-15 09:21:01 +0300

    Refactor __call__ with cache
    +        if n not in instance.__dict__[self.cache]:
    +            val = self.func(*args)
    +            instance._cache[n] = val

--- lazycall.py
+++ lazycall.py
@@ -7,27 +7,32 @@ from types import MethodType
 class lazycall:
     def __init__(self, func):
         self.func = func
+        self.cache = "_cache"
 
     def __get__(self, instance, owner=None):
         if instance is None:
             return self
+        if self.cache not in instance.__dict__:
+            instance.__dict__[self.cache] = {}
         return MethodType(self, instance)
 
     def __call__(self, *args):
-        return self.func(*args)
+        # args = (<__main__.Fib object at 0x7feb8a8242d0>, 6)
+        instance, n = args
+        if n not in instance.__dict__[self.cache]:
+            val = self.func(*args)
+            instance._cache[n] = val
+        return instance._cache[n]
 
 
 class Fib:
     @lazycall
     def fib(self, n):
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            return self.fib(n - 1) + self.fib(n - 2)
+        print(f"Calculating fibonacci({n})")
+        return self.fib(n - 1) + self.fib(n - 2) if n >= 2 else n
 
 
 if __name__ == "__main__":
     f = Fib()
     print(f.fib(7))
+    print(f.fib(8))
